1cbd38cd664fbe7947ed602fd48a12051d061228
Lume03 <jorge.luquem@unmsm.edu.pe>
2025-04-21 11:07:28 -0500

    Restaurar archivos del backend y agregar configuración

---
+++ backend-ecommerce/config/server.ts
@@ -0,0 +1,12 @@
+// config/server.ts
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),   // Asegura que Strapi esté accesible desde cualquier host
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  allowedHosts: [
+    'rnzfp-38-25-26-75.a.free.pinggy.link', // Agrega el dominio de Pinggy aquí
+    'localhost', // Asegúrate de permitir localhost también
+  ],
+});
