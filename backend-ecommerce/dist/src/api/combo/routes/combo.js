e1951090318a4a1334f9be62690143d4192c143d
Lume03 <jorge.luquem@unmsm.edu.pe>
2025-04-21 10:10:56 -0500

    Añadir backend-ecommerce

---
+++ backend-ecommerce/dist/src/api/combo/routes/combo.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * combo router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::combo.combo');
